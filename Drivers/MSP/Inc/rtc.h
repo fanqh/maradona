8986f7e4a13fc6ff8b8e3711670ec507e5c008c5
xujing <jingle_sue@actnova.com>
2014-07-25 18:35:59 +0800

    add rtc.c just save my work

---
+++ Drivers/MSP/Inc/rtc.h
@@ -0,0 +1,44 @@
+
+#ifndef __RTC_H
+#define __RTC_H
+#include "stm32f4xx_hal.h"
+
+
+typedef struct
+{
+		RTC_DateTypeDef initsdatestructure;
+		RTC_TimeTypeDef initstimestructure;
+	
+}RTC_CalendarTypeDef;
+
+typedef enum
+{
+	 RTCEX_STATE_RESET,
+	 RTCEX_STATE_SET,
+	 RTCEX_STATE_CONFIG
+}RTC_State_TypeDef;
+
+typedef struct
+{
+	RTC_HandleTypeDef rtc;
+	RTC_CalendarTypeDef calendar;
+	RTC_State_TypeDef state;
+}RTCEX_HandleTypeDef; 
+
+typedef struct
+{
+	HAL_StatusTypeDef	(*init)(RTC_HandleTypeDef *hrtc);
+	HAL_StatusTypeDef	(*deinit)(RTC_HandleTypeDef *hrtc);
+	HAL_StatusTypeDef (*settime)(RTC_HandleTypeDef *hrtc, RTC_TimeTypeDef *sTime, uint32_t Format);
+	HAL_StatusTypeDef (*setdata)(RTC_HandleTypeDef *hrtc, RTC_DateTypeDef *sDate, uint32_t Format);
+	HAL_StatusTypeDef (*gettime)(RTC_HandleTypeDef *hrtc, RTC_TimeTypeDef *sTime, uint32_t Format);
+	HAL_StatusTypeDef (*getdata)(RTC_HandleTypeDef *hrtc, RTC_TimeTypeDef *sTime, uint32_t Format);
+	
+}RTCEX_Operations;
+
+
+#endif
+
+
+
+
---
+++ Drivers/MSP/Src/rtc.c
@@ -0,0 +1,100 @@
+#include "stm32f4xx_hal.h"
+#include "rtc.h"
+#include <string.h>
+
+
+
+const RTCEX_HandleTypeDef	RTC_Haldle_Default=
+{
+	.rtc =
+	{
+		.Instance = RTC,
+		.Init =
+		{
+				.HourFormat = RTC_HOURFORMAT_24,
+				.AsynchPrediv = 0x7F,		/* LSE as RTC clock */
+				.SynchPrediv = 0x00FF,  /* LSE as RTC clock */
+				.OutPut = RTC_OUTPUT_DISABLE,
+				.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH,
+				.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN,
+		},
+	},
+//	.calendar = 
+//	{
+//			.initsdatestructure =
+//			{
+//					.Year = 0x14,
+//					.Month = RTC_MONTH_JULY,
+//					.Date = 0x24,
+//					.WeekDay = RTC_WEEKDAY_MONDAY,
+//			},
+//			.initstimestructure = 
+//			{
+//					.Hours = 0x00,
+//					.Minutes = 0x00,
+//					.Seconds =0x00,
+//				
+//			},
+//	}
+	
+};
+
+const RTC_CalendarTypeDef InitCalendar =
+{
+			.initsdatestructure =
+			{
+					.Year = 0x14,
+					.Month = RTC_MONTH_JULY,
+					.Date = 0x7,
+					.WeekDay = RTC_WEEKDAY_MONDAY,
+			},
+			.initstimestructure = 
+			{
+					.Hours = 0x00,
+					.Minutes = 0x00,
+					.Seconds =0x00,		
+			}	
+};
+HAL_StatusTypeDef RTC_CalendarConfig(RTCEX_HandleTypeDef* hRTChandle,RTC_CalendarTypeDef calendar)
+{
+	HAL_StatusTypeDef state;
+	RTC_HandleTypeDef *phadle = &hRTChandle->rtc;
+	
+	state = HAL_RTC_SetDate(phadle,&calendar.initsdatestructure,FORMAT_BCD);
+	if(state!=HAL_OK)
+			return state;
+	state = HAL_RTC_SetDate(phadle,&calendar.initstimestructure,FORMAT_BCD);
+	if (state!=HAL_OK)
+			return state;
+		
+		
+	
+}
+
+int RTC_open(unsigned char *name ,int flage)
+{
+		if(strcmp(name, "RTC1")!=0)
+				return -1;
+		if(HAL_RTC_Init((RTC_HandleTypeDef*) &RTC_Haldle_Default.rtc)!=HAL_OK)
+					return -1;
+		
+}	
+int RTC_write(RTCEX_HandleTypeDef rtc, void *buff, unsigned int len)
+{
+		
+}
+
+
+int RTC_read(RTCEX_HandleTypeDef rtc, void *buff, unsigned int len)
+{
+}
+
+
+int RTC_close(RTCEX_HandleTypeDef rtc)
+{
+	
+}
+
+
+
+
