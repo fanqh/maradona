933b3f5e2b0850a2e76508df2611837a3f87a495
xujing <jingle_sue@actnova.com>
2014-07-28 16:33:50 +0800

    add some in rtc.c

--- Drivers/MSP/Src/rtc.c
+++ Drivers/MSP/Src/rtc.c
@@ -4,6 +4,10 @@
 
 
 
+
+#define BKUFLAGE  0X32F2
+
+
 const RTCEX_HandleTypeDef	RTC_Haldle_Default=
 {
 	.rtc =
@@ -19,23 +23,6 @@ const RTCEX_HandleTypeDef	RTC_Haldle_Default=
 				.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN,
 		},
 	},
-//	.calendar = 
-//	{
-//			.initsdatestructure =
-//			{
-//					.Year = 0x14,
-//					.Month = RTC_MONTH_JULY,
-//					.Date = 0x24,
-//					.WeekDay = RTC_WEEKDAY_MONDAY,
-//			},
-//			.initstimestructure = 
-//			{
-//					.Hours = 0x00,
-//					.Minutes = 0x00,
-//					.Seconds =0x00,
-//				
-//			},
-//	}
 	
 };
 
@@ -55,6 +42,9 @@ const RTC_CalendarTypeDef InitCalendar =
 					.Seconds =0x00,		
 			}	
 };
+
+
+
 HAL_StatusTypeDef RTC_CalendarConfig(RTCEX_HandleTypeDef* hRTChandle,RTC_CalendarTypeDef calendar)
 {
 	HAL_StatusTypeDef state;
@@ -63,9 +53,8 @@ HAL_StatusTypeDef RTC_CalendarConfig(RTCEX_HandleTypeDef* hRTChandle,RTC_Calenda
 	state = HAL_RTC_SetDate(phadle,&calendar.initsdatestructure,FORMAT_BCD);
 	if(state!=HAL_OK)
 			return state;
-	state = HAL_RTC_SetDate(phadle,&calendar.initstimestructure,FORMAT_BCD);
-	if (state!=HAL_OK)
-			return state;
+	state = HAL_RTC_SetTime(phadle,&calendar.initstimestructure,FORMAT_BCD);
+	return state;
 		
 		
 	
@@ -73,7 +62,7 @@ HAL_StatusTypeDef RTC_CalendarConfig(RTCEX_HandleTypeDef* hRTChandle,RTC_Calenda
 
 int RTC_open(unsigned char *name ,int flage)
 {
-		if(strcmp(name, "RTC1")!=0)
+		if(strcmp((const char*) name, "RTC1")!=0)
 				return -1;
 		if(HAL_RTC_Init((RTC_HandleTypeDef*) &RTC_Haldle_Default.rtc)!=HAL_OK)
 					return -1;
