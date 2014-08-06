edcd478c5e5e33e10f9536fbdb2bf963e6feb329
matianfu <matianfu@gmail.com>
2014-08-06 22:39:50 +0800

    dmaex handle ctor/dtor/factory cleaned.

--- Drivers/MSP/Test/dma_test.c
+++ Drivers/MSP/Test/dma_test.c
@@ -87,29 +87,7 @@ TEST_GROUP_RUNNER(DMA_Clock)
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-//typedef struct {
-//	
-//	DMA_ClockProviderTypeDef							*clk;
-//	IRQ_HandleRegistryTypeDef			*reg;
-//	
-//} DMAEX_Handle_FactoryTypeDef;
 
-//DMAEX_HandleTypeDef*	DMAEX_Handle_FactoryCreate(DMAEX_Handle_FactoryTypeDef* factory, 
-//																									const DMA_HandleTypeDef* hdma,
-//																									const IRQ_HandleTypeDef* hirq);
-
-//const DMA_InitTypeDef DMA_Init_Uart2Rx =
-//{
-//	.Channel = DMA_CHANNEL_4,
-//	.Direction = DMA_PERIPH_TO_MEMORY,
-//	.PeriphInc = DMA_PINC_DISABLE,
-//	.MemInc = DMA_MINC_ENABLE,
-//	.PeriphDataAlignment = DMA_PDATAALIGN_BYTE,
-//	.MemDataAlignment = DMA_MDATAALIGN_BYTE,
-//	.Mode = DMA_NORMAL,
-//	.Priority = DMA_PRIORITY_LOW,
-//	.FIFOMode = DMA_FIFOMODE_DISABLE,
-//};
 
 TEST_GROUP(DMAEX_Handle);
 TEST_SETUP(DMAEX_Handle)
@@ -119,33 +97,6 @@ TEST_SETUP(DMAEX_Handle)
 TEST_TEAR_DOWN(DMAEX_Handle)
 {	
 }
-
-//TEST(DMAEX_Handle, Ctor)
-//{
-//	const DMA_HandleTypeDef* dfl = &DMA_Handle_Uart2Rx_Default;
-//	DMA_ClockProviderTypeDef clk;
-//	IRQ_HandleTypeDef	irq;
-
-//	DMAEX_HandleTypeDef* h = DMAEX_Handle_Ctor(dfl->Instance, &dfl->Init, &clk, &irq);
-//	TEST_ASSERT_NOT_NULL(h);
-//	TEST_ASSERT_EQUAL_HEX32(&clk, h->clk);
-//	TEST_ASSERT_EQUAL_HEX32(dfl->Instance, h->hdma.Instance);
-//	TEST_ASSERT_EQUAL_MEMORY(&dfl->Init, &h->hdma.Init, sizeof(dfl->Init));
-//	TEST_ASSERT_EQUAL_HEX32(&irq, h->hirq);
-//	TEST_ASSERT_EQUAL(DMAEX_HANDLE_STATE_RESET, h->state);
-//	
-//	// all other field should be zero
-//	TEST_ASSERT_EQUAL(0, h->hdma.ErrorCode);
-//	TEST_ASSERT_EQUAL(0, h->hdma.Lock);
-//	TEST_ASSERT_EQUAL(0, h->hdma.Parent);
-//	TEST_ASSERT_EQUAL(0, h->hdma.State);
-//	TEST_ASSERT_EQUAL(0, h->hdma.XferCpltCallback);
-//	TEST_ASSERT_EQUAL(0, h->hdma.XferErrorCallback);
-//	TEST_ASSERT_EQUAL(0, h->hdma.XferHalfCpltCallback);
-//	TEST_ASSERT_EQUAL(0, h->hdma.XferM1CpltCallback);
-//	
-//	if (h) free(h);
-//}
 
 /** int DMAEX_Handle_Init(DMAEX_HandleTypeDef* h, DMA_Stream_TypeDef *stream, const DMA_InitTypeDef *init,
 	DMA_ClockProviderTypeDef *clk, IRQ_HandleTypeDef *hirq); **/
@@ -207,7 +158,7 @@ TEST(DMAEX_Handle, HandleInitSuccess)
 /**
 int DMAEX_Handle_InitByConfig(DMAEX_HandleTypeDef* h, const DMA_ConfigTypeDef* config, 
 	DMA_ClockProviderTypeDef *clk, IRQ_HandleTypeDef *hirq); **/
-TEST(DMAEX_Handle, InitByConfigInvalidArgs)
+TEST(DMAEX_Handle, HandleInitByConfigInvalidArgs)
 {
 	int ret;
 	
@@ -249,63 +200,6 @@ TEST(DMAEX_Handle, HandleInitByConfigSuccess)
 	TEST_ASSERT_EQUAL(0, h.hdma.XferM1CpltCallback);
 }
 
-TEST(DMAEX_Handle, FactoryCreate)
-{
-	DMA_ClockProviderTypeDef			clk;
-	IRQ_HandleRegistryTypeDef			registry;
-	DMA_ConfigTypeDef							dma_config;
-	IRQ_ConfigTypeDef							irq_config;
-
-	DMAEX_HandleTypeDef*					h;
-	
-	memset(&dma_config, 0xA5, sizeof(dma_config));
-	memset(&irq_config, 0xB5, sizeof(irq_config));
-	irq_config.irqn = USART2_IRQn;				/** must be something valid **/
-	dma_config.Instance = DMA1_Stream5; 	/** must be something valid **/
-	
-	h = DMAEX_Handle_FactoryCreate(&clk, &registry, &dma_config, &irq_config);
-	
-	TEST_ASSERT_NOT_NULL(h);
-	TEST_ASSERT_EQUAL_HEX32(&clk, h->clk);
-	
-	TEST_ASSERT_EQUAL_HEX32(dma_config.Instance, h->hdma.Instance);
-	TEST_ASSERT_EQUAL_MEMORY(&dma_config.Init, &h->hdma.Init, sizeof(DMA_InitTypeDef));
-
-	TEST_ASSERT_EQUAL(irq_config.irqn, h->hirq->irqn);
-	TEST_ASSERT_EQUAL(irq_config.preempt_priority, h->hirq->preempt_priority);
-	TEST_ASSERT_EQUAL(irq_config.sub_priority, h->hirq->sub_priority);
-	TEST_ASSERT_EQUAL(&registry, h->hirq->registry);
-	TEST_ASSERT_EQUAL(IRQ_HANDLE_STATE_RESET, h->hirq->state);
-	
-	TEST_ASSERT_EQUAL(DMAEX_HANDLE_STATE_RESET, h->state);
-	
-	if (h->hirq) free(h->hirq);
-	if (h) free(h);
-}
-
-//TEST(DMAEX_Handle, FactoryDestroy)
-//{
-//	DMA_ClockProviderTypeDef					clk;
-//	IRQ_HandleRegistryTypeDef					registry;
-//	
-//	DMA_HandleTypeDef									hdma;
-//	DMA_ConfigTypeDef									dma_config;
-//	IRQ_HandleTypeDef									hirq;
-//	IRQ_ConfigTypeDef									irq_config;
-
-//	DMAEX_HandleTypeDef*							h;
-//	
-//	memset(&hdma, 0xA5, sizeof(hdma));
-//	memset(&hirq, 0xB5, sizeof(hirq));
-
-//	hirq.state = IRQ_HANDLE_STATE_RESET;
-
-//	h = DMAEX_Handle_FactoryCreate(&clk, &registry, &dma_config, &irq_config);
-//	DMAEX_Handle_FactoryDestroy(h);
-//}
-
-
-
 TEST(DMAEX_Handle, DMAEX_HAL_Init)
 {
 	DMAEX_HandleTypeDef hdmaex;
@@ -326,19 +220,11 @@ TEST(DMAEX_Handle, DMAEX_HAL_Init)
 	TEST_ASSERT_EQUAL(&hdmaex.hdma, hdmaex.hirq->irqh_obj);
 	TEST_ASSERT_EQUAL(IRQ_HANDLE_STATE_SET, hdmaex.hirq->state);
 	TEST_ASSERT_EQUAL(DMAEX_HANDLE_STATE_SET, hdmaex.state);
-	
-	// DMAEX_Handle_Dtor(hdmaex);
+
 }
 
 TEST(DMAEX_Handle, DMAEX_HAL_DeInit)
 {
-//	IRQ_HandleTypeDef irq;
-//	IRQ_Handle_Init(&irq, DMA1_Stream5_IRQn, 0, 0, &IRQ_HandlerObjectRegistry);
-//	
-//	DMAEX_HandleTypeDef* hdmaex = DMAEX_Handle_Ctor(DMAEX_Handle_Uart2Rx_Default.hdma.Instance, &DMAEX_Handle_Uart2Rx_Default.hdma.Init, &DMA_ClockProvider, &irq);
-//	
-//	DMAEX_HAL_Init(hdmaex);
-//	DMAEX_HAL_DeInit(hdmaex);
 	DMAEX_HandleTypeDef hdmaex;
 	IRQ_HandleTypeDef irq;
 	
@@ -358,19 +244,14 @@ TEST(DMAEX_Handle, DMAEX_HAL_DeInit)
 	TEST_ASSERT_NULL(hdmaex.hirq->irqh_obj);
 	TEST_ASSERT_EQUAL(IRQ_HANDLE_STATE_RESET, hdmaex.hirq->state);
 	TEST_ASSERT_EQUAL(DMAEX_HANDLE_STATE_RESET, hdmaex.state);
-	
-	// DMAEX_Handle_Dtor(hdmaex);
 }
 
 TEST_GROUP_RUNNER(DMAEX_Handle)
 {
 	RUN_TEST_CASE(DMAEX_Handle, HandleInitInvalidArgs);
 	RUN_TEST_CASE(DMAEX_Handle, HandleInitSuccess);
-	RUN_TEST_CASE(DMAEX_Handle, InitByConfigInvalidArgs);
+	RUN_TEST_CASE(DMAEX_Handle, HandleInitByConfigInvalidArgs);
 	RUN_TEST_CASE(DMAEX_Handle, HandleInitByConfigSuccess);
-	
-	RUN_TEST_CASE(DMAEX_Handle, FactoryCreate);
-	// RUN_TEST_CASE(DMAEX_Handle, FactoryDestroy);
 	
 	RUN_TEST_CASE(DMAEX_Handle, DMAEX_HAL_Init);
 	RUN_TEST_CASE(DMAEX_Handle, DMAEX_HAL_DeInit);
